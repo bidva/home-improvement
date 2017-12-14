6833c5350ccc2062f7e56cdfbfe2c9f814cbd297
amir bidva <a.bidva@gmail.com>
2017-12-14 13:01:59 -0500

    addind some styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap/dropdown
-//= require global
 //= require turbolinks
+//= require bootstrap/dropdown
+//= require bootstrap-sprockets
+//= require_tree .
