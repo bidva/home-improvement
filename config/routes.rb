64c57ff1da1994ce404126268bb67487556aa641
amir bidva <a.bidva@gmail.com>
2017-12-22 08:27:45 -0500

    adding static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  get 'static_pages/home'
+  get 'static_pages/about'
+
   devise_for :users, 
   	:controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :projects do
